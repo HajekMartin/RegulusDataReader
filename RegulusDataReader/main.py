c33fab63398f1c01b5166c2de3af85e080c5f488
Martin <martin.hajek1@tul.cz>
2023-12-10 13:49:26 +0100

    test

--- RegulusDataReader/main.py
+++ RegulusDataReader/main.py
@@ -1,1 +1,15 @@
+import yaml
+
+with open('config.yaml', 'r') as config_file:
+    config = yaml.safe_load(config_file)
+
+# Find the 'username' option in the list
+username_option = next((item for item in config['options'] if item['name'] == 'username'), None)
+
+# Print the details of the 'username' option
+if username_option is not None:
+    print(username_option)
+else:
+    print("Username option not found")
+
 print(f"Test print!")
