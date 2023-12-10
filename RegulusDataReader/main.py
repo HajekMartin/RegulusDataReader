9a9606bfc2b148c0400d473e288f13f2612f938b
Martin <martin.hajek1@tul.cz>
2023-12-10 15:38:14 +0100

    starting of dev

--- RegulusDataReader/main.py
+++ RegulusDataReader/main.py
@@ -1,28 +1,16 @@
 import yaml
-import os
+import time
 
-folder_path = '/usr/src/app'
+#file_path = '/usr/src/app'
+file_path = './RegulusDataReader/config.yaml'
 
-# Get the list of files and directories in the folder
-folder_content = os.listdir(folder_path)
-
-# Print the folder content
-for item in folder_content:
-    print(item)
-
-
-with open('/usr/src/app/config.yaml', 'r') as config_file:
+with open(file_path, 'r') as config_file:
     config = yaml.safe_load(config_file)
 
-external_hostname = next((item for item in config['options'] if item['name'] == 'external_hostname'), None)
-print(external_hostname)
-additional_hosts = next((item for item in config['options'] if item['name'] == 'additional_hosts'), None)
-print(additional_hosts)
+interval = config['options'].get('interval')
 
-# Print the details of the 'username' option
-if external_hostname is not None:
-    print(external_hostname)
-else:
-    print("Username option not found")
+print("interval:", interval)
 
-print(f"Test print!")
+while True:
+    print("I'm alive!")
+    time.sleep(interval)
