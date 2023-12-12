7e322b968b23b2cb06b40d48d3e5a8d604e7e678
Martin Hájek <hajek-martin@email.cz>
2023-12-12 17:22:01 +0100

    Update interval for XML reading

--- RegulusDataReader/main.py
+++ RegulusDataReader/main.py
@@ -6,7 +6,7 @@ file_path = '/usr/src/app/config.yaml'
 #file_path = './RegulusDataReader/config.yaml'
 
 # Global
-interval = 1
+interval = 60
 
 def read_config():
     with open(file_path, 'r') as config_file:
@@ -31,7 +31,6 @@ if __name__ == '__main__':
 
     while True:
         print(f"Reading XML at {time.strftime('%Y-%m-%d %H:%M:%S')}")
-        read_xml()
-
+        #read_xml()
         time.sleep(interval)
     
