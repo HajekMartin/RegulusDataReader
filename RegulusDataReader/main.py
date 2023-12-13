969a765d9371013133a7a403364450a9478d6d46
Martin <martin.hajek1@tul.cz>
2023-12-13 12:52:45 +0100

    improved and tested

--- RegulusDataReader/main.py
+++ RegulusDataReader/main.py
@@ -2,28 +2,28 @@ import yaml
 import time
 import requests
 
-print("Starting RegulusDataReader...")
-"""
-Reading config
-"""
-file_path = '/usr/src/app/config.yaml'
-#file_path = './RegulusDataReader/config.yaml'
-with open(file_path, 'r') as config_file:
-    config = yaml.safe_load(config_file)
+def read_config():
+    file_path = '/usr/src/app/config.yaml'
+    #file_path = './RegulusDataReader/config.yaml'
+    with open(file_path, 'r') as config_file:
+        config = yaml.safe_load(config_file)
 
-interval = config['options'].get('interval')
-print("interval:", interval)
+    interval = config['options'].get('interval')
+    print("interval:", interval)
 
-
-"""
-While loop
-"""
-url = 'https://regulusroute.tecomat.com/HOME.XML'
+def read_xml():
+    url = 'https://regulusroute.tecomat.com/HOME.XML'
     
-print(f"Reading XML {time}")
-response = requests.get(url)
-xml_content = response.text
+    print(f"Reading XML {time}")
+    response = requests.get(url)
+    xml_content = response.text
 
-# Print XML content
-print(xml_content)
-time.sleep(interval)
+    # Print XML content
+    print(xml_content)
+
+if __name__ == '__main__':
+    read_config()
+    while True:
+        read_xml()
+
+        time.sleep(60)
