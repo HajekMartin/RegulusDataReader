245b8d20ba9461152315061295f718b988a0b57c
Martin <martin.hajek1@tul.cz>
2023-12-13 09:39:14 +0100

    yaml repair

--- RegulusDataReader/main.py
+++ RegulusDataReader/main.py
@@ -2,7 +2,7 @@ import yaml
 import time
 
 if __name__=='__main__':
-    file_path = '/usr/src/app'
+    file_path = '/usr/src/app/config.yaml'
     #file_path = './RegulusDataReader/config.yaml'
 
     with open(file_path, 'r') as config_file:
