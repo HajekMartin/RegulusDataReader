9886d4e353074cd8af848ce69812c1409e8f96f3
Martin Hájek <hajek-martin@email.cz>
2023-12-14 12:23:26 +0100

    try catch

--- RegulusDataReader/main.py
+++ RegulusDataReader/main.py
@@ -4,28 +4,30 @@ import requests
 import datetime
 
 def read_config():
-    file_path = '/usr/src/app/config.yaml'
-    #file_path = './RegulusDataReader/config.yaml'
+    #file_path = '/usr/src/app/config.yaml'
+    file_path = './RegulusDataReader/config.yaml'
     with open(file_path, 'r') as config_file:
         config = yaml.safe_load(config_file)
 
     interval = config['options'].get('interval')
     print("interval:", interval)
 
-def read_xml():
-    url = 'http://192.168.0.116/HOME.XML'
-    
+def read_xml(url):
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"Reading XML at {current_time}")
+    print(f"Reading URL {url} at {current_time}")
     response = requests.get(url)
     xml_content = response.text
-
+    
     # Print XML content
     print(xml_content)
 
 if __name__ == '__main__':
     read_config()
     while True:
-        read_xml()
-
+        try:
+            url = 'http://192.168.0.116/HOME.XML'
+            read_xml(url)
+        except Exception as e: 
+            print("Error reading URL:", e)
+        
         time.sleep(10)
