47bcd7d8c77990fbdef451fc75428b146f142532
Martin <martin.hajek1@tul.cz>
2024-02-02 10:31:38 +0100

    test of post temps

--- RegulusDataReader/main.py
+++ RegulusDataReader/main.py
@@ -1,33 +1,78 @@
-import yaml
-import time
 import requests
+import json
+import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
+import os
 import datetime
 
-def read_config():
-    file_path = '/usr/src/app/config.yaml'
-    #file_path = './RegulusDataReader/config.yaml'
-    with open(file_path, 'r') as config_file:
-        config = yaml.safe_load(config_file)
+DEF_FODLER = 'usr/src/app/'
 
-    interval = config['options'].get('interval')
-    print("interval:", interval)
+def send_temperatures():
+    url = read_config("url")
+    guid = read_config("guid")
+    regulus_url = read_config("regulus_url")
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    regulus_url = regulus_url + "/SDC/Datalog/teploty/" + yesterday.strftime('%Y/%m/%d') + "000000.CSV"
 
-def read_xml(url):
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"Reading URL {url} at {current_time}")
-    response = requests.get(url)
-    xml_content = response.text
+    # Download CSV file
+    print_log("Downloading CSV file from " + regulus_url, "send_temperatures")
+    response = requests.get(regulus_url)
+    if response.status_code == 200:
+        print_log("CSV file downloaded successfully", "send_temperatures")
+        csv_data = response.content
 
-    # Print XML content
-    print(xml_content)
+        # Send CSV data via POST
+        data = {'csvData': csv_data, 'guid': guid, 'date': yesterday.strftime('%Y-%m-%d')}
+        response = requests.post(url, data=data)
+        if response.status_code == 200:
+            print_log("CSV file sent successfully", "send_temperatures")
+        else:
+            print_log("Failed to send CSV file", "send_temperatures")
+    else:
+        print_log("Failed to download CSV file", "send_temperatures")
+
+    
+
+def read_config(value_name):
+    try:
+        print_log("Reading config " + value_name, "read_config")
+        file_path = 'data/' + 'options.json'
+        
+        with open(file_path) as file:
+            data = json.load(file)
+            if value_name in data:
+                value = data[value_name]
+                print_log("Config " + value_name + " = " + value, "read_config")
+                return value
+            else:
+                raise Exception("Value not found")
+    except Exception as e:
+        print_log("Failed to read config: " + str(e), "read_config")
+        return None
+
+def print_log(message, method):
+    string = "[ " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ] " + method + " " + message
+    create_log_file()
+    
+    log_file = DEF_FODLER + 'logs/' + datetime.datetime.now().strftime("%Y-%m-%d") + '.log'
+    with open(log_file, 'a') as file:
+        file.write(string + '\n')
+    
+def create_log_file():
+    log_folder = DEF_FODLER + 'logs/'
+    if not os.path.exists(log_folder):
+        os.makedirs(log_folder)
+    
+    log_file = log_folder + datetime.datetime.now().strftime("%Y-%m-%d") + '.log'
+    if not os.path.exists(log_file):
+        with open(log_file, 'w') as file:
+            pass
 
 if __name__ == '__main__':
-    read_config()
-    while True:
-        try:
-            url = 'http://192.168.0.116/HOME.XML'
-            read_xml(url)
-        except Exception as e: 
-            print("Error reading URL:", e)
+    print_log("Starting RegulusDataReader", "main")
+    send_temperatures()
+    scheduler = BlockingScheduler()
+    scheduler.add_job(send_temperatures, 'cron', hour=1, minute=0)
 
-        time.sleep(10)
+    scheduler.start()
+    
