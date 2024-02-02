aa2504023e97ba7e46cfd6eb1b5d11d9fd6fb00c
Martin <martin.hajek1@tul.cz>
2024-02-02 21:38:08 +0100

    new vers

--- RegulusDataReader/main.py
+++ RegulusDataReader/main.py
@@ -4,32 +4,63 @@ import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 import os
 import datetime
+import xml.etree.ElementTree as ET
 
 DEF_FODLER = 'usr/src/app/'
 
+def download_heated_zones():
+    try:
+        print_log("Downloading heated zones", "download_heated_zones")
+        url = read_config("heated_zones_list_url")
+        response = requests.get(url)
+        data = response.json()
+        save_to_file(data)  # Save the heated zones to a file
+        print_log("Heated zones downloaded", "download_heated_zones")
+        return data
+    except Exception as e:
+        print_log("Failed to download heated zones: " + str(e), "download_heated_zones")
+        return None
+
+def save_to_file(data):
+    file_path = DEF_FODLER + 'heated_zones.json'
+    with open(file_path, 'w') as file:
+        file.write(json.dumps(data))
+        json.dump(data, file)
+        print_log("Heated zones saved to file", "save_to_file")
+
+def read_from_file(file_name):
+    try:
+        print_log("Reading from file " + file_name, "read_from_file")
+        file_path = DEF_FODLER + file_name
+        with open(file_path) as file:
+            data = json.load(file)
+            print_log("Data read from file", "read_from_file")
+            return data
+    except Exception as e:
+        print_log("Failed to read from file: " + str(e), "read_from_file")
+        return None
+
 def send_temperatures():
-    url = read_config("url")
-    guid = read_config("guid")
-    regulus_url = read_config("regulus_url")
-    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-    regulus_url = regulus_url + "SDC/Datalog/teploty/" + yesterday.strftime('%Y/%m/%d') + "000000.CSV"
-
-    # Download CSV file
-    print_log("Downloading CSV file from " + regulus_url, "send_temperatures")
-    response = requests.get(regulus_url)
-    if response.status_code == 200:
-        print_log("CSV file downloaded successfully", "send_temperatures")
-        csv_data = response.content
-
-        # Send CSV data via POST
-        data = {'csvData': csv_data, 'guid': guid, 'date': yesterday.strftime('%Y-%m-%d')}
-        response = requests.post(url, data=data)
-        if response.status_code == 200:
-            print_log("CSV file sent successfully", "send_temperatures")
-        else:
-            print_log("Failed to send CSV file", "send_temperatures")
-    else:
-        print_log("Failed to download CSV file: " + str(response.content), "send_temperatures")  
+    guid = read_config('guid')
+    heated_zones_temperatures_url = read_config('heated_zones_temperatures_url')
+    try:
+        print_log("Sending temperatures", "send_temperatures")
+        heated_zones = read_from_file('heated_zones.json')
+        for heated_zone in heated_zones:
+            url = heated_zone['url']
+            response = requests.get(url)
+            xml_data = response.text
+            root = ET.fromstring(xml_data)
+            diag = root.find(".//*[@name='{}']".format(heated_zone['code']))
+            post_data = {
+                'guid': guid,
+                'code': heated_zone['code'],
+                'attribute_value': diag.attrib['value']
+            }
+            response = requests.post(heated_zones_temperatures_url, data=post_data)
+            print_log("Temperature sent for zone: " + heated_zone['code'], "send_temperatures")
+    except Exception as e:
+        print_log("Failed to send temperatures: " + str(e), "send_temperatures")
 
 def read_config(value_name):
     try:
@@ -68,9 +99,11 @@ def create_log_file():
 
 if __name__ == '__main__':
     print_log("Starting RegulusDataReader", "main")
-    send_temperatures()
+
+    download_heated_zones()
+
     scheduler = BlockingScheduler()
-    scheduler.add_job(send_temperatures, 'cron', hour=1, minute=0)
+    scheduler.add_job(send_temperatures, 'cron', minute='0,15,30,45')
 
     scheduler.start()
     
